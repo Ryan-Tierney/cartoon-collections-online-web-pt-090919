3ee0a01ef2f6fef5e5cc006f21783802a428f16e
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-10-18 06:54:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,23 +1,3 @@
-def roll_call_dwarves(dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"])
-  dwarves.each_with_index  do |dwarf, index|
-   puts "#{index} #{dwarf}" 
- end 
-end
-
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
-
-
 function dwarfRollCall(dwarves) {
   var newStr = "";
   dwarves.forEach(function(name, idx) {
