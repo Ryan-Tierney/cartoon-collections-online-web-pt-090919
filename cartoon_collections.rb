acdbaef6843c5bab6b5b51a260af3e6728658ecb
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-10-18 06:52:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"])
-  dwarves.each_with_index(1)  do |dwarf, index|
+  dwarves.each_with_index  do |dwarf, index|
    puts "#{index} #{dwarf}" 
  end 
 end
